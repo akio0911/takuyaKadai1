ee0cb2ae1997453866b6e17d523ef8ed56f9c98f
takuya119 <takuya.nakahara.119@gmail.com>
2022-09-04 23:04:10 +0900

    UIとコードの紐付け

--- kadai1/ViewController.swift
+++ kadai1/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fieldOne: UITextField!
+    @IBOutlet weak var fieldTwo: UITextField!
+    @IBOutlet weak var fieldThree: UITextField!
+    @IBOutlet weak var fieldFour: UITextField!
+    @IBOutlet weak var fieldFive: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
 }
 
